c474569b8ecea53965bb9619d9675b23f481eebf
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2021-09-18 23:27:26 +0200

    Napravlje main klasa Main1 kao ispomoc pri pisanju koda. Napraljena i implementirana klasa SOVratiBrojPratilaca. Dodate neophodne zavisnosti u pom.xml fajlu.

--- src/main/java/rs/ac/bg/fon/ai/MavenBlog/SistemskeOperacije/SOPostaviBlogPost.java
+++ src/main/java/rs/ac/bg/fon/ai/MavenBlog/SistemskeOperacije/SOPostaviBlogPost.java
@@ -2,6 +2,12 @@ package rs.ac.bg.fon.ai.MavenBlog.SistemskeOperacije;
 
 public class SOPostaviBlogPost {
 
+	
+	private static final String BASE_URL ="api.tumblr.com/v2/blog";
+	private static final String API_KEY ="QcfVaxZBTRF4Z0e03I8pTRAPV4OLmxnzWVtyZff2R5azVBhpKl";
+	private static final String BLOG_IDENTIFIER = "https://www.tumblr.com/student-engineer";
+	
+	
 public static void postaviBlog() {
 		
 		
