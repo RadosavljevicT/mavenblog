ed70bda46b516ec95db201b9b8b8ddbffebda807
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2021-09-20 00:25:34 +0200

    Implementirana klasa SOObrisiBlogPost

--- src/main/java/rs/ac/bg/fon/ai/MavenBlog/SistemskeOperacije/SOObrisiBlogPost.java
+++ src/main/java/rs/ac/bg/fon/ai/MavenBlog/SistemskeOperacije/SOObrisiBlogPost.java
@@ -1,9 +1,20 @@
 package rs.ac.bg.fon.ai.MavenBlog.SistemskeOperacije;
 
+import com.tumblr.jumblr.JumblrClient;
+
 public class SOObrisiBlogPost {
 
-public static void obrisiBlog() {
-		
+public static void obrisiBlogPost(String s1, String s2, long postId) {
+	
+	JumblrClient client = new JumblrClient(
+			  "QcfVaxZBTRF4Z0e03I8pTRAPV4OLmxnzWVtyZff2R5azVBhpKl",
+			  "TKDxnP0l1JgGP6eOKmeUGzSu6v8Mow5siUpoYyeoMdkJsZpSVG"
+			);
+			
+	 
+			client.setToken(s1,s2);
+			
+			client.postDelete("student-engineer.tumblr.com",postId);
 		
 	}
 	
