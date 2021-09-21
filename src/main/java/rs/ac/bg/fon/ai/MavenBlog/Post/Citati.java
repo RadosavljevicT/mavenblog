ae70fcb17ab58e4add926fdb810f862887031ad4
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2021-09-22 01:37:43 +0200

    Dokumentovana klasa Citati.

--- src/main/java/rs/ac/bg/fon/ai/MavenBlog/Post/Citati.java
+++ src/main/java/rs/ac/bg/fon/ai/MavenBlog/Post/Citati.java
@@ -4,24 +4,57 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import rs.ac.bg.fon.ai.MavenBlog.Interface.BlogInterface;
-
+/**
+ * Klasa koja predstavlja citate i nasledjuje interfejs BlogInterface.
+ * @author Tamara Radosavljevic
+ *
+ */
 public class Citati implements BlogInterface {
 	
+	/**
+	 * Ime autora citata kao String.
+	 */
 	String imeAutora;
+	
+	/**
+	 * Citat kao String.
+	 */
 	String citat;
 	
+	/**
+	 * Metoda koja vraca ime autora citata.
+	 * @return ime  ime autora citata kao String.
+	 */
 	public String getImeAutora() {
 		return imeAutora;
 	}
+	
+	/**
+	 * Metoda koja postavlja ime autora citata na novu vrednost.
+	 * @param ime ime autora citata kao String.
+	 * @throws java.lang.NullPointerException ako je uneto ime null.
+	 */
+
 	public void setImeAutora(String imeAutora) {
 		if (imeAutora == null)
 			throw new NullPointerException ("Ime autora bloga ne sme biti null!");
 		
 		this.imeAutora = imeAutora;
 	}
+	
+	/**
+	 * Metoda koja vraca citat.
+	 * @return citat citat kao String.
+	 */
 	public String getCitat() {
 		return citat;
 	}
+	
+	/**
+	 * Metoda koja postavlja citata na novu vrednost.
+	 * @param citat citat kao String.
+	 * @throws java.lang.NullPointerException ako je vrednost citata null.
+	 */
 	public void setCitat(String citat) {
 		
 		if (citat == null)
@@ -30,6 +63,9 @@ public class Citati implements BlogInterface {
 		this.citat = citat;
 	}
 	
+	/**
+	 * Metoda koja prikazuje citate u formatu: ime autora + citat
+	 */
 	@Override
 	public void prikazi() {
 	
