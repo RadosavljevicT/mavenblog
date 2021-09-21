4108d941c174937dce24fadca1e85dc5d5124222
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2021-09-22 00:57:29 +0200

    Dokumentovana klasa Autor.

--- src/main/java/rs/ac/bg/fon/ai/MavenBlog/Autor/Autor.java
+++ src/main/java/rs/ac/bg/fon/ai/MavenBlog/Autor/Autor.java
@@ -1,18 +1,39 @@
 package rs.ac.bg.fon.ai.MavenBlog.Autor;
 
 import rs.ac.bg.fon.ai.MavenBlog.Interface.BlogInterface;
-
+/**
+ * Klasa koja predstavlja autora blog posta.
+ * Autor ima ime i prezime kao String vrednosti
+ * @author Tamara Radosavljevic
+ * @version 0.1
+ *
+ */
 public class Autor implements BlogInterface {
 
+	/**
+	 * Ime autora kao String.
+	 */
 	String ime;
+	/**
+	 * Prezime autora kao String.
+	 */
 	String prezime;
 	
-	
+	/**
+	 * Metoda koja vraca ime autora.
+	 * @return ime  ime autora kao String.
+	 */
 	
 	public String getIme() {
 		
 		return ime;
 	}
+	
+	/**
+	 * Metoda koja postavlja ime autora na novu vrednost.
+	 * @param ime ime autora kao String.
+	 * @throws java.lang.NullPointerException ako je uneto ime null.
+	 */
 	public void setIme(String ime) {
 		
 		if (ime == null)
@@ -20,10 +41,22 @@ public class Autor implements BlogInterface {
 		
 		this.ime = ime;
 	}
+	
+	/**
+	 * Metoda koja vraca prezime autora.
+	 * @return prezime prezime autora kao String.
+	 *
+	 */
 	public String getPrezime() {
 		
 		return prezime;
 	}
+	
+	/**
+	 * Metoda koja postavlja prezime autora na novu vrednost.
+	 * @param prezime prezime autora kao String.
+	 * @throws java.lang.NullPointerException ako je uneto prezime null.
+	 */
 	public void setPrezime(String prezime) {
 		
 		if (prezime == null)
@@ -31,6 +64,9 @@ public class Autor implements BlogInterface {
 		
 		this.prezime = prezime;
 	}
+	/**
+	 * Metoda koja ispisuje prezime i ime autora.
+	 */
 	@Override
 	public void prikazi() {
 		
