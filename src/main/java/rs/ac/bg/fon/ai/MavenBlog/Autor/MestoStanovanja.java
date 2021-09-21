3bd74938e71eca21b4ca78f7141161d8f2edc141
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2021-09-22 01:08:24 +0200

    Dokumentovana klasa MestoStanovanja.

--- src/main/java/rs/ac/bg/fon/ai/MavenBlog/Autor/MestoStanovanja.java
+++ src/main/java/rs/ac/bg/fon/ai/MavenBlog/Autor/MestoStanovanja.java
@@ -1,15 +1,39 @@
 package rs.ac.bg.fon.ai.MavenBlog.Autor;
 
+/**
+ * Klasa koja predstavlja mesto stanovanja autora.
+ * @author Tamara Radosavljevic
+ *
+ */
 public class MestoStanovanja {
-	
+	/**
+	 * Postanski broj mesta stanovanja autora kao int.
+	 */
 	int postanskiBrojMesta;
+	
+	/**
+	 * Naziv mesta stanovanja autora kao String.
+	 */
 	String nazivMesta;
+	
+	/**
+	 * Naziv drzave u kojoj autor zivi kao String.
+	 */
 	String nazivDrzave;
 	
-	
+	/**
+	 * Metoda koja vraca postanski broj mesta stanovanja.
+	 * @return postanskiBrojMesta postanski broj mesta kao int.
+	 */
 	public int getPostanskiBrojMesta() {
 		return postanskiBrojMesta;
 	}
+	
+	/**
+	 * Metoda koja postavlja postanski broj mesta stanovanja na novu vrednost.
+	 * @param postanskiBrojMesta postanski broj stanovanja kao int.
+	 * @throws java.lang.RuntimeException ako je postanski broj mesta manji od nule.
+	 */
 	public void setPostanskiBrojMesta(int postanskiBrojMesta) {
 		
 		if (postanskiBrojMesta<0)
@@ -17,9 +41,20 @@ public class MestoStanovanja {
 		
 		this.postanskiBrojMesta = postanskiBrojMesta;
 	}
+	
+	/**
+	 * Metoda koja vraca naziv mesta stanovanja.
+	 * @return nazivMesta naziv mesta stanovanja kao String.
+	 */
 	public String getNazivMesta() {
 		return nazivMesta;
 	}
+	
+	/**
+	 * Metoda koja postavlja naziv mesta na novu vrednost.
+	 * @param nazivMesta naziv mesta stanovanja kao String.
+	 * @throws java.lang.NullPointerException ako je naziv mesta null.
+	 */
 	public void setNazivMesta(String nazivMesta) {
 		
 		if (nazivMesta ==null)
@@ -27,9 +62,20 @@ public class MestoStanovanja {
 		
 		this.nazivMesta = nazivMesta;
 	}
+	
+	/**
+	 * Metoda koja vraca naziv drzave u kojoj autor zivi.
+	 * @return nazivDrzave naziv drzave kao String.
+	 */
 	public String getNazivDrzave() {
 		return nazivDrzave;
 	}
+	
+	/**
+	 * Metoda koja postavlja naziv drzave na novu vrednosti.
+	 * @param nazivDrzave naziv drzave u kojoj autor zivi kao String.
+	 * @throws java.long.NullPointerException ako je naziv drzave null.
+	 */
 	public void setNazivDrzave(String nazivDrzave) {
 		
 		if (nazivDrzave ==null)
