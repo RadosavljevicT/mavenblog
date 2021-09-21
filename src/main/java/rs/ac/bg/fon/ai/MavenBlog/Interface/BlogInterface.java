2974a11ff56b89520c99c7acb5e5e038c3390b33
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2021-09-22 01:21:52 +0200

    Dokumentovan interfejs BlogInterface.

--- src/main/java/rs/ac/bg/fon/ai/MavenBlog/Interface/BlogInterface.java
+++ src/main/java/rs/ac/bg/fon/ai/MavenBlog/Interface/BlogInterface.java
@@ -1,9 +1,15 @@
 package rs.ac.bg.fon.ai.MavenBlog.Interface;
 
 import rs.ac.bg.fon.ai.MavenBlog.Post.Post;
-
+/**
+ * Metoda koja predstavlja interfejs bloga i sluzi za prikazivanje podataka.
+ * @author Tamara Radosavljevic
+ *
+ */
 public interface BlogInterface {
-	
+	/**
+	 * Metoda koja sluzi za prikazivanje podataka.
+	 */
 	public void prikazi ();
 	
 	
