ed7ca3f6ea9b3e975f1804c208b9aeb908656902
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2021-09-22 03:00:17 +0200

    Dokumentovana klasa SOVratiTekst.

--- src/main/java/rs/ac/bg/fon/ai/MavenBlog/SistemskeOperacije/SOVratiTekst.java
+++ src/main/java/rs/ac/bg/fon/ai/MavenBlog/SistemskeOperacije/SOVratiTekst.java
@@ -2,9 +2,18 @@ package rs.ac.bg.fon.ai.MavenBlog.SistemskeOperacije;
 
 import java.util.Scanner;
 
+/**
+ * Klasa koja predstavlja sistemsku operaciju za vracanje teksta.
+ * @author Tamara Radosavljevic
+ *
+ */
 public class SOVratiTekst {
 
-	
+	/**
+	 * Metoda koja vraca tekst. Tekst se ucitava preko tastature.
+	 * @return str tekst kao String.
+	 * @throws java.lang.NullPointerException ako je vrednost teksta null.
+	 */
 	public static String vratiTekst() {
 		
 		
