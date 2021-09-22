0a0cc9c1cfe3d09f8d5b3d6f101e890ed0328bcd
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2021-09-22 02:55:38 +0200

    Dokumentovana klasa SOVratiNaslov.

--- src/main/java/rs/ac/bg/fon/ai/MavenBlog/SistemskeOperacije/SOVratiNaslov.java
+++ src/main/java/rs/ac/bg/fon/ai/MavenBlog/SistemskeOperacije/SOVratiNaslov.java
@@ -2,8 +2,17 @@ package rs.ac.bg.fon.ai.MavenBlog.SistemskeOperacije;
 
 import java.util.Scanner;
 
+/**
+ * Klasa koja predstavlja sistemsku operaciju za vracanje naslova objave.
+ * @author Tamara Radosavljevic
+ *
+ */
 public class SOVratiNaslov {
-	
+	/**
+	 * Metoda koja vraca naslov objave. Naslov se ucitava preko tastature.
+	 * @return tekst tekst o uspesnosti kao String.
+	 * @throws java.lang.NullPointerException ako je vrednost naslova null.
+	 */
 	public static String vratiNaslov() {
 		
 		Scanner sc= new Scanner(System.in); 
