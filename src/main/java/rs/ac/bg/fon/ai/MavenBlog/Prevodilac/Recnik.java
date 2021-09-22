2fd0ec20e9db19dbe98fd2515e5e41c24313fe4e
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2021-09-22 02:06:00 +0200

    Dokumentovana klasa Recnik.

--- src/main/java/rs/ac/bg/fon/ai/MavenBlog/Prevodilac/Recnik.java
+++ src/main/java/rs/ac/bg/fon/ai/MavenBlog/Prevodilac/Recnik.java
@@ -1,12 +1,30 @@
 package rs.ac.bg.fon.ai.MavenBlog.Prevodilac;
-
+/**
+ * Klasa koja predstavlja recnik znacenja reci.
+ * @author Tamara Radosavljevic
+ *
+ */
 public class Recnik {
 	
+	/**
+	 * Rec kao String.
+	 */
 	String rec;
 
+	
+	/**
+	 * Metoda koja vraca rec.
+	 * @return rec rec kao String.
+	 */
 	public String getRec() {
 		return rec;
 	}
+	
+	/**
+	 * Metoda koja postavlja rec na novu vrednost.
+	 * @param rec rec kao String.
+	 * @throws java.lang.NullPointerException ako je rec null.
+	 */
 
 	public void setRec(String rec) {
 		
